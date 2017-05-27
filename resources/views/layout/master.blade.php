83b25722c4329f374b4d161460e287d6c5a60cf2
Bonnak <chea.bonnak@gmail.com>
2017-05-27 22:08:09 +0700

    Extract top navigation

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,46 +8,13 @@
     <title>Angkor Pool</title>
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"  integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-    <style>
-        body {
-            padding-top: 70px;
-        }
-    </style>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <link rel="stylesheet" href="{{ url('css/font.css') }}">
+    <link rel="stylesheet" href="{{ url('css/bulma.css') }}">
+    <link rel="stylesheet" href="{{ url('css/app.css') }}">
 </head>
 <body>
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
-                    aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Angkor Pool</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="/">Home</a></li>
-                <li><a href="/messages/create">Create New Message</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
-<div class="container">
-    @yield('content')
-</div>
+@include('partial.top-navigation')
+@yield('content')
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
---
+++ resources/views/partial/top-navigation.blade.php
@@ -0,0 +1,36 @@
+<nav class="nav has-shadow">
+    <div class="container">
+        <div class="nav-left">
+            <a class="nav-item is-brand" href="{{ url('/') }}">
+                <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox">
+            </a>
+        </div>
+
+        <span id="nav-toggle" class="nav-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </span>
+
+        <div id="nav-menu" class="nav-right nav-menu">
+            <a class="nav-item" href="{{ url('employer/post/job') }}">Post job</a>
+
+            <div class="nav-item">
+                <div class="field is-grouped">
+                    <p class="control">
+                        <a id="twitter" class="button is-primary" href="{{ url('user/login') }}">
+                            <span class="icon"><i class="fa fa-key"></i></span>
+                            <span>Login</span>
+                        </a>
+                    </p>
+                    <p class="control">
+                        <a class="button is-primary" href="{{ url('user/register') }}">
+                            <span class="icon"><i class="fa fa-user"></i></span>
+                            <span>Register</span>
+                        </a>
+                    </p>
+              </div>
+            </div>
+        </div>
+    </div>
+</nav>
