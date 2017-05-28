6df6f0b0e8c2228a0862dbff830c3646a3e9d13c
Bonnak <chea.bonnak@gmail.com>
2017-05-28 12:02:12 +0700

    Hide login and register button if user authenticated

--- resources/views/partial/top-navigation.blade.php
+++ resources/views/partial/top-navigation.blade.php
@@ -15,22 +15,24 @@
         <div id="nav-menu" class="nav-right nav-menu">
             <a class="nav-item" href="{{ url('employer/post/job') }}">Post job</a>
 
-            <div class="nav-item">
-                <div class="field is-grouped">
-                    <p class="control">
-                        <a id="twitter" class="button is-primary" href="{{ url('user/login') }}">
-                            <span class="icon"><i class="fa fa-key"></i></span>
-                            <span>Login</span>
-                        </a>
-                    </p>
-                    <p class="control">
-                        <a class="button is-primary" href="{{ url('user/register') }}">
-                            <span class="icon"><i class="fa fa-user"></i></span>
-                            <span>Register</span>
-                        </a>
-                    </p>
-              </div>
-            </div>
+            @if(!auth()->check())
+                <div class="nav-item">
+                    <div class="field is-grouped">
+                        <p class="control">
+                            <a id="twitter" class="button is-primary" href="{{ url('user/login') }}">
+                                <span class="icon"><i class="fa fa-key"></i></span>
+                                <span>Login</span>
+                            </a>
+                        </p>
+                        <p class="control">
+                            <a class="button is-primary" href="{{ url('user/register') }}">
+                                <span class="icon"><i class="fa fa-user"></i></span>
+                                <span>Register</span>
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 </nav>
