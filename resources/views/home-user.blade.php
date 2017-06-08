0989bb8fb368af1a2f57a780e55ecd23b4a06faa
Bonnak <chea.bonnak@gmail.com>
2017-06-08 16:06:46 +0700

    View job on user dashboard

--- resources/views/home-user.blade.php
+++ resources/views/home-user.blade.php
@@ -1,8 +1,35 @@
 @extends('layout.master')
 
 @section('content')
-<section class="section top-most-margin">
-	<div class="container">
-	</div>
-</section>
+<div class="container top-most-margin">
+    <div class="tabs is-boxed">
+        <ul>
+            <li class="is-active"><a>Latest Jobs</a></li>
+            <li><a>Applied Jobs</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="container bg-white border-left border-right border-bottom">
+    <section class="section">
+        <table class="table">
+            <thead>
+                <tr>
+                    <td>Title</td>
+                    <td>Location</td>
+                    <td>Closing Date</td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($jobs as $job)
+                    <tr>
+                        <td><a href="{{ url('job/1')}}">{{ $job->title }}</a></td>
+                        <td>{{ $job->title }}</td>
+                        <td>{{ $job->closing_date }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </section>
+</div>
 @stop
