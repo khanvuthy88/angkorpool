83f7e10bbbaf557290709224d990dde6d4f3c01b
Bonnak <chea.bonnak@gmail.com>
2017-06-09 15:38:31 +0700

    Create employer table and model

---
+++ database/migrations/2017_06_09_083052_create_employers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('contact_number')->nullable();
+            $table->string('fax')->nullable();
+            $table->integer('industry_id')->unsigned()->nullable();
+            $table->string('website')->nullable();
+            $table->text('about')->nullable();
+            $table->string('street')->nullable();
+            $table->string('city')->nullable();
+            $table->string('province')->nullable();
+            $table->string('post_code')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employers');
+    }
+}
