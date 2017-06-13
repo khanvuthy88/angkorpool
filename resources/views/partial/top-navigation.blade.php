4951137b4df40583b5f44635ccfa8ca7b57035bb
Bonnak <chea.bonnak@gmail.com>
2017-06-13 11:48:38 +0700

    Remove padding l r

--- resources/views/partial/top-navigation.blade.php
+++ resources/views/partial/top-navigation.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-toggleable-md shadow20 zindex-navbar">
         <div class="container">
             <div class="row full-width no-margin">
-                <div class="col-lg-2 col-md-12">
+                <div class="col-lg-2 col-md-12 pl-lg-0">
                     <a href="{{ url('/') }}" class="navbar-brand">
                         <img src="{{ url('storage/logo/Angkor-Pool-logo-s-1.png') }}">
                     </a>
@@ -18,7 +18,7 @@
                         <span></span>
                     </button>
                 </div>
-                <div class="col-lg-8 hidden-md-down">
+                <div class="col-lg-7 hidden-md-down">
                     <div class="collapse navbar-collapse">
                         <ul class="navbar-nav align-items-center">
                             @if(auth()->check())
@@ -41,7 +41,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-lg-2 hidden-md-down">
+                <div class="col-lg-3 hidden-md-down pr-lg-0">
                     <div class="collapse navbar-collapse justify-content-end">
                         <ul class="navbar-nav align-items-center">
                             @if(auth()->check())
