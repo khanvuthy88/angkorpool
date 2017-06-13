08516535a7242832cd3df6c77050e58e519c8b89
Bonnak <chea.bonnak@gmail.com>
2017-06-13 11:49:06 +0700

    Style alert and shadow button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,52 +2,52 @@
 
 @section('content')
 <section class="section">
-    <div class="container d-flex justify-content-center">
-        <div class="col-md-12 col-lg-7">
-            <div class="card">
-                <h5 class="card-header"><i class="fa fa-key mr-2"></i>Login</h5>
-                <div class="card-block">
-                    <form action="/user/login" method="POST">
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-12 col-lg-7">
+                <div class="card">
+                    <h5 class="card-header"><i class="fa fa-key mr-2"></i>Login</h5>
+                    <div class="card-block">
                         @if($errors->has('credential'))
-                            <p class="help has-text-centered is-danger">
-                                {{ $errors->first('credential') }}
-                            </p>
+                            <div class="alert alert-danger" id="alert">{{ $errors->first('credential') }}</div>
                         @endif
-                        {{ csrf_field() }}
-                        <div class="form-group row {{ ! $errors->has('email') ?: 'has-danger' }}">
-                          <label for="email" class="col-sm-12 col-md-3 col-form-label">Email</label>
-                          <div class="col-sm-12 col-md-9">
-                            <input class="form-control" type="email" id="email" name="email" value="{{ old('email') }}">
-                            @if ($errors->has('email'))
-                                <div class="form-control-feedback">{{ $errors->first('email') }}</div>
-                            @endif
-                          </div>
-                        </div>
-                        <div class="form-group row {{ ! $errors->has('password') ?: 'has-danger' }}">
-                          <label for="password" class="col-sm-12 col-md-3 col-form-label">Password</label>
-                          <div class="col-sm-12 col-md-9">
-                            <input class="form-control" type="password" id="password" name="password" value="{{ old('password') }}">
-                            @if ($errors->has('password'))
-                                <div class="form-control-feedback">{{ $errors->first('password') }}</div>
-                            @endif
-                          </div>
-                        </div>
-                        <div class="form-group row">
-                            <div class="col-sm-12 col-md-9 offset-md-3">
-                                <div class="form-check">
-                                    <label class="form-check-label">
-                                      <input type="checkbox" class="form-check-input" name="remember">
-                                      Remember Me
-                                    </label>
+                        <form action="/user/login" method="POST">
+                            {{ csrf_field() }}
+                            <div class="form-group row {{ ! $errors->has('email') ?: 'has-danger' }}">
+                              <label for="email" class="col-sm-12 col-md-3 col-form-label">Email</label>
+                              <div class="col-sm-12 col-md-9">
+                                <input class="form-control" type="email" id="email" name="email" value="{{ old('email') }}">
+                                @if ($errors->has('email'))
+                                    <div class="form-control-feedback"><small>{{ $errors->first('email') }}</small></div>
+                                @endif
+                              </div>
+                            </div>
+                            <div class="form-group row {{ ! $errors->has('password') ?: 'has-danger' }}">
+                              <label for="password" class="col-sm-12 col-md-3 col-form-label">Password</label>
+                              <div class="col-sm-12 col-md-9">
+                                <input class="form-control" type="password" id="password" name="password" value="{{ old('password') }}">
+                                @if ($errors->has('password'))
+                                    <div class="form-control-feedback"><small>{{ $errors->first('password') }}</small></div>
+                                @endif
+                              </div>
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-sm-12 col-md-9 offset-md-3">
+                                    <div class="form-check">
+                                        <label class="form-check-label">
+                                          <input type="checkbox" class="form-check-input" name="remember">
+                                          Remember Me
+                                        </label>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="form-group row mb-0">
-                            <div class="col-sm-12 col-md-9 offset-md-3 d-flex flex-column flex-md-row">
-                                <button class="btn btn-secondary">Login</button>
+                            <div class="form-group row mb-0">
+                                <div class="col-sm-12 col-md-9 offset-md-3 d-flex flex-column flex-md-row">
+                                    <button class="btn btn-secondary">Login</button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
