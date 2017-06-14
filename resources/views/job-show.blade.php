518dd4cd89e06fe982c2ecc5f0515c06330f830a
Bonnak <chea.bonnak@gmail.com>
2017-06-14 12:39:11 +0700

    Set font weight 600

--- resources/views/job-show.blade.php
+++ resources/views/job-show.blade.php
@@ -11,27 +11,27 @@
             <table class="table">
                 <tbody>
                     <tr>
-                        <td class="font-weight-bold width-min-200">Job Type</td>
+                        <td class="font-weight-600 width-min-200">Job Type</td>
                         <td><p class="card-text">{{ $job->job_type }}</p></td>
                     </tr>
                     <tr>
-                        <td class="font-weight-bold width-min-200">No. Of Positions</td>
+                        <td class="font-weight-600 width-min-200">No. Of Positions</td>
                         <td><p class="card-text">{{ $job->number_of_positions }}</p></td>
                     </tr>
                     <tr>
-                        <td class="font-weight-bold width-min-200">Location</td>
+                        <td class="font-weight-600 width-min-200">Location</td>
                         <td><p class="card-text">{{ $job->province }}</p></td>
                     </tr>
                     <tr>
-                        <td class="font-weight-bold width-min-200">Industry</td>
+                        <td class="font-weight-600 width-min-200">Industry</td>
                         <td><p class="card-text">{{ $job->industry }}</p></td>
                     </tr>
                     <tr>
-                        <td class="font-weight-bold width-min-200">Experience</td>
+                        <td class="font-weight-600 width-min-200">Experience</td>
                         <td><p class="card-text">{{ $job->work_experience }}</p></td>
                     </tr>
                     <tr>
-                        <td class="font-weight-bold width-min-200">Job Description</td>
+                        <td class="font-weight-600 width-min-200">Job Description</td>
                         <td><p class="card-text">{{ $job->description }}</p></td>
                     </tr>
                 </tbody>
@@ -45,27 +45,27 @@
             <h6 class="card-subtitle mb-2 text-muted font-italic">Posted on {{ \Carbon\Carbon::parse($job->published_date)->format('Y-m-d') }}</h6>
 
             <dl class="mt-4">
-                <dt>Job Type</dt><hr class="mt-1 mb-1">
+                <dt class="font-weight-600">Job Type</dt><hr class="mt-1 mb-1">
                 <dd><p class="card-text">{{ $job->job_type }}</p></dd>
             </dl>
             <dl class="mt-4">
-                <dt>No. Of Positions</dt><hr class="mt-1 mb-1">
+                <dt class="font-weight-600">No. Of Positions</dt><hr class="mt-1 mb-1">
                 <dd><p class="card-text">{{ $job->number_of_positions }}</p></dd>
             </dl>
             <dl class="mt-4">
-                <dt>Location</dt><hr class="mt-1 mb-1">
+                <dt class="font-weight-600">Location</dt><hr class="mt-1 mb-1">
                 <dd><p class="card-text">{{ $job->province }}</p></dd>
             </dl>
             <dl class="mt-4">
-                <dt>Industry</dt><hr class="mt-1 mb-1">
+                <dt class="font-weight-600">Industry</dt><hr class="mt-1 mb-1">
                 <dd><p class="card-text">{{ $job->industry }}</p></dd>
             </dl>
             <dl class="mt-4">
-                <dt>Experience</dt><hr class="mt-1 mb-1">
+                <dt class="font-weight-600">Experience</dt><hr class="mt-1 mb-1">
                 <dd><p class="card-text">{{ $job->work_experience }}</p></dd>
             </dl>
             <dl class="mt-4">
-                <dt>Job Description</dt><hr class="mt-1 mb-1">
+                <dt class="font-weight-600">Job Description</dt><hr class="mt-1 mb-1">
                 <dd><p class="card-text">{{ $job->description }}</p></dd>
             </dl>
         </div>
