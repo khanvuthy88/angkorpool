a9c45a5e12174cb43690ba7ab79c5846ec2cbff5
Bonnak <chea.bonnak@gmail.com>
2017-06-14 11:41:19 +0700

    Job show responsvie

--- resources/views/job-show.blade.php
+++ resources/views/job-show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="card">
+    <div class="card hidden-sm-down">
         <div class="card-block">
             <h4 class="card-title">{{ $job->title }}</h4>
             <h6 class="card-subtitle mb-2 text-muted font-italic">Posted on {{ \Carbon\Carbon::parse($job->published_date)->format('Y-m-d') }}</h6>
@@ -37,6 +37,40 @@
                 </tbody>
             </table>
             <a href="#" class="btn btn-secondary text-uppercase pull-right">Apply</a>
+        </div>
+    </div>
+    <div class="card hidden-md-up">
+        <div class="card-block">
+            <h4 class="card-title">{{ $job->title }}</h4>
+            <h6 class="card-subtitle mb-2 text-muted font-italic">Posted on {{ \Carbon\Carbon::parse($job->published_date)->format('Y-m-d') }}</h6>
+
+            <dl class="mt-4">
+                <dt>Job Type</dt><hr class="mt-1 mb-1">
+                <dd><p class="card-text">{{ $job->job_type }}</p></dd>
+            </dl>
+            <dl class="mt-4">
+                <dt>No. Of Positions</dt><hr class="mt-1 mb-1">
+                <dd><p class="card-text">{{ $job->number_of_positions }}</p></dd>
+            </dl>
+            <dl class="mt-4">
+                <dt>Location</dt><hr class="mt-1 mb-1">
+                <dd><p class="card-text">{{ $job->province }}</p></dd>
+            </dl>
+            <dl class="mt-4">
+                <dt>Industry</dt><hr class="mt-1 mb-1">
+                <dd><p class="card-text">{{ $job->industry }}</p></dd>
+            </dl>
+            <dl class="mt-4">
+                <dt>Experience</dt><hr class="mt-1 mb-1">
+                <dd><p class="card-text">{{ $job->work_experience }}</p></dd>
+            </dl>
+            <dl class="mt-4">
+                <dt>Job Description</dt><hr class="mt-1 mb-1">
+                <dd><p class="card-text">{{ $job->description }}</p></dd>
+            </dl>
+        </div>
+        <div class="card-block">
+            <a href="#" class="btn btn-secondary text-uppercase full-width">Apply</a>
         </div>
     </div>
 </div>
