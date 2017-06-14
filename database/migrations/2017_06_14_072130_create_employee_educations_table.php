e33b94e8da438ddd77c3417719e57ddd699412c4
Bonnak <chea.bonnak@gmail.com>
2017-06-14 14:30:41 +0700

    Migrate user experience and education

---
+++ database/migrations/2017_06_14_072130_create_employee_educations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmployeeEducationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employee_educations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('title');
+            $table->string('college');
+            $table->date('from_date');
+            $table->date('to_date');
+            $table->text('extra_detail')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employee_educations');
+    }
+}
