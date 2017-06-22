9780fa84b4527f62ec4a26814f8a220eeea59987
Bonnak <chea.bonnak@gmail.com>
2017-06-22 22:23:25 +0700

    Set employee default guard

--- config/auth.php
+++ config/auth.php
@@ -14,8 +14,8 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web.users',
-        'passwords' => 'users',
+        'guard' => 'web.employees',
+        'passwords' => 'employees',
     ],
 
     /*
